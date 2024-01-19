3fe6c737d9a7cfa9ff07afbfa4f5d6092ac0c0c8
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-19 19:42:51 -0300

    feat: add register form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
 			<Link className='absolute right-5 top-5 font-medium text-primary' href='/register'>
 				Cadastre-se
 			</Link>
-			<section className='mx-auto flex h-screen w-full max-w-[444px] items-center justify-center'>
+			<section className='mx-auto flex h-screen w-full max-w-[444px] items-center justify-center px-2 lg:px-0'>
 				<LoginForm />
 			</section>
 		</main>
---
+++ src/features/auth/dtos/register-form-dto.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const RegisterFormDTO = z.object({
+	name: z.string().min(1, { message: 'Esse campo precisa ser preenchido' }),
+	email: z
+		.string()
+		.min(1, { message: 'Esse campo precisa ser preenchido' })
+		.email('Precisa ser um email valido'),
+	password: z
+		.string()
+		.min(1, { message: 'Esse campo precisa ser preenchido' })
+		.regex(new RegExp('^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$ %^&*-]).{8,}$'), {
+			message:
+				'A senha precisa ter no mínimo 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caracter especial'
+		})
+})
+
+export type RegisterFormData = z.infer<typeof RegisterFormDTO>
