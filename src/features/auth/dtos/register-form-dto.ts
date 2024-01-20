fb2a853ba91220078d732d15819c794f01a3d0f8
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-20 17:12:19 -0300

    feat: add msw (#18)

--- src/features/auth/dtos/register-form-dto.ts
+++ src/features/auth/dtos/register-form-dto.ts
@@ -9,10 +9,10 @@ export const RegisterFormDTO = z.object({
 	password: z
 		.string()
 		.min(1, { message: 'Esse campo precisa ser preenchido' })
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-      message:
-        'A senha precisa ter no mínimo 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caracter especial',
-    })
+		.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+			message:
+				'A senha precisa ter no mínimo 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caracter especial'
+		})
 })
 
 export type RegisterFormData = z.infer<typeof RegisterFormDTO>
