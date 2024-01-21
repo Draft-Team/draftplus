cdca68263bfaba665c320c079f2feb67be0b81e3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-21 16:12:02 -0300

    refactor: add more mocks to login

--- src/shared/mocks/login/resolvers.ts
+++ src/shared/mocks/login/resolvers.ts
@@ -5,13 +5,47 @@ interface LoginBody {
 	password: string
 }
 
+interface LoginResponse {
+	accessToken: {
+		id: string
+		name: string
+		email: string
+	}
+}
+
+const users = [
+	{
+		id: '1',
+		name: 'User',
+		email: 'user@email.com',
+		password: 'user-password'
+	},
+	{
+		id: '2',
+		name: 'User 2',
+		email: 'user2@email.com',
+		password: 'user2-password'
+	}
+]
+
 export const loginResolver: ResponseResolver = async ({ request }) => {
 	const body = (await request.json()) as LoginBody
-	return HttpResponse.json({
-		user: {
-			id: 'abc-123',
-			email: body.email,
-			password: body.password
+
+	const user = users.find(
+		(user) => user.email === body.email && user.password === body.password
+	)
+
+	if (!user) {
+		return new HttpResponse(null, { status: 401 })
+	}
+
+	const token: LoginResponse = {
+		accessToken: {
+			id: user.id,
+			name: user.name,
+			email: user.email
 		}
-	})
+	}
+
+	return HttpResponse.json(token)
 }
