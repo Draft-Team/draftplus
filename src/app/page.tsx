b6a48c384d503e95a4dba2e3e572764472d85d98
Vitor <68834970+vitorFRE@users.noreply.github.com>
2024-01-27 01:03:04 -0400

    feat:add share section (#35)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,10 +27,57 @@ export default async function Page() {
 				</section>
 			</main>
 
+			<section className='container mx-auto mt-20'>
+				<h2 className='text-3xl font-semibold'>Descubra, Crie e Compartilhe</h2>
+				<div className='relative mt-16 grid grid-cols-1 justify-between gap-4 lg:grid-cols-2'>
+					<div>
+						<figure className='relative h-[300px] overflow-hidden rounded-md lg:h-[400px]  lg:max-w-[586px]'>
+							<BlurImage
+								className='h-full w-full object-fill'
+								src='/chef.jpg'
+								fill
+								placeholder='blur'
+								blurDataURL={placeholderBlurhash}
+								alt='DraftPlus Logo'
+							/>
+						</figure>
+					</div>
+					<div>
+						<span className='text-3xl font-semibold'>Compartilhe suas receitas</span>
+						<p className='mt-2 max-w-[361px] font-medium text-muted-foreground'>
+							De forma simples e rápida você compartilha suas receitas
+						</p>
+
+						<div className='mt-6 flex  flex-col gap-4'>
+							<div className='flex items-center gap-4'>
+								<div className='inline-flex h-16 w-16 items-center justify-center rounded-full bg-primary/5 p-3 text-center text-primary shadow-lg'>
+									<span className='text-2xl font-semibold'>1</span>
+								</div>
+								<p>Crie uma conta</p>
+							</div>
+							<hr className='h-1' />
+							<div className='flex items-center gap-4'>
+								<div className='inline-flex h-16 w-16 items-center justify-center rounded-full bg-primary/5 p-3 text-center text-primary shadow-lg'>
+									<span className='text-2xl font-semibold'>2</span>
+								</div>
+								<p>Crie uma receita</p>
+							</div>
+							<hr className='h-1' />
+							<div className='flex items-center gap-4'>
+								<div className='inline-flex h-16 w-16 items-center justify-center rounded-full bg-primary/5 p-3 text-center text-primary shadow-lg'>
+									<span className='text-2xl font-semibold'>3</span>
+								</div>
+								<p>Publique</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
 			<section className='mt-20 border-y bg-[url("/food-cta.jpg")] bg-cover bg-no-repeat bg-blend-overlay dark:bg-[#333]  md:bg-none dark:md:bg-[#141414]'>
 				<div className='container mx-auto grid grid-cols-1 items-center md:grid-cols-2 md:pr-0'>
 					<div className='max-w-[463px] py-7 md:py-0'>
-						<h2 className='text-3xl font-semibold'>Descubra nova receitas</h2>
+						<h3 className='text-3xl font-semibold'>Descubra nova receitas</h3>
 						<p className='mb-8 mt-8 font-medium text-muted-foreground'>
 							Explorar novos sabores na cozinha é como desvendar um mundo de
 							possibilidades onde a criatividade se torna o tempero principal para
