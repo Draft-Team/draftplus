27b5cf8ebcfeadafa970542d2235848ded6dcfda
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-03-02 21:41:03 -0300

    refactor: modify project structure and libs

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -2,7 +2,10 @@ import { z } from "zod"
 
 const envSchema = z.object({
 	// Node variables
-	NODE_ENV: z.enum(["development", "production", "test"])
+	NODE_ENV: z.enum(["development", "production", "test"]),
+
+	// Backend variables
+	BACKEND_URL: z.string()
 })
 
 export const env = envSchema.parse(process.env)
